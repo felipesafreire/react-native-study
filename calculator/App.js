b932a3901ec5e0bd176aeec921d3a29f57ea1e3b
Luis Freire <luis.freire@ftdata.com.br>
2019-02-15 13:22:37 -0200

    Logica e finalização da calculadora

--- calculator/App.js
+++ calculator/App.js
@@ -3,20 +3,70 @@ import { Platform, StyleSheet, Text, View } from "react-native";
 import Button from "./src/componentes/Button";
 import Display from "./src/componentes/Display";
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
+
 export default class App extends Component {
+
+  state = { ...initialState }
+
   state = {
     displayValue: "0"
   };
 
   addDigit = n => {
-    this.setState({ displayValue: n });
+
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+
+    if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')) {
+      return;
+    }
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay: false })
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+      this.state({ values })
+    }
+
   };
 
   clearMemory = () => {
-    this.setState({ displayValue: "0" });
+    this.setState({ ...initialState })
   };
 
-  setOperation = operation => {};
+  setOperation = operation => {
+    if (this.state.currentValue == 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try {
+        values[0] = eval(`${values[0]} ${operation} ${values[1]}`)
+      } catch (e) {
+        values[0] = this.state.values[0]
+      }
+
+      values[1] = 0
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        // clearDisplay: !equals,
+        clearDisplay: true,
+        values
+      })
+    }
+  };
 
   render() {
     return (
@@ -38,8 +88,8 @@ export default class App extends Component {
           <Button label="3" onClick={this.addDigit} />
           <Button label="+" operation onClick={this.setOperation} />
           <Button label="0" double onClick={this.addDigit} />
-          <Button label="." onClick={this.addDigit}/>
-          <Button label="="  operation onClick={this.setOperation} />
+          <Button label="." onClick={this.addDigit} />
+          <Button label="=" operation onClick={this.setOperation} />
         </View>
       </View>
     );
