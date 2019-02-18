6fada6c28ee055a6b13cc7aaf0f8c821ad67c37e
Luis Freire <luis.freire@ftdata.com.br>
2019-02-18 12:46:32 -0300

    Componente field - projeto mines part 1

---
+++ mines/src/components/Field.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, StyleSheet } from 'react-native';
+import params from "../params";
+
+export default props => {
+    const styleField = [style.field]
+    if(styleField.length === 1) styleField.push(style.regular)
+}
