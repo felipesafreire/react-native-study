3116a0d4e7b5ce23f80cff5bb69f093c87ea22fa
Luis Freire <luis.freire@ftdata.com.br>
2019-02-19 13:06:48 -0300

    Componente Mina

--- mines/App.js
+++ mines/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params';
+import Field from './src/components/Field';
 
 export default class App extends Component {
   render() {
@@ -10,6 +11,14 @@ export default class App extends Component {
         <Text style={styles.instructions}> Tamanho da grade:
           {params.getRowsAmount()}x{params.getCollumnsAmount()}
         </Text>
+        <Field />
+        <Field opened nearMines={1}></Field>
+        <Field opened nearMines={2}></Field>
+        <Field opened nearMines={3}></Field>
+        <Field opened nearMines={6}></Field>
+        <Field mined></Field>
+        <Field mined opened></Field>
+        <Field mined opened exploded></Field>
       </View>
     );
   }
