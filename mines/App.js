d147ebae15cfb08e0d03e0b593bcca9f8b755396
Luis Freire <luis.freire@ftdata.com.br>
2019-02-28 13:06:42 -0300

    Mine Field - Campo minado

--- mines/App.js
+++ mines/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params';
-import Field from './src/components/Field';
+import MineField from './src/components/MineField';
+import {
+  createMinedBoard
+} from './src/functions'
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getCollumnsAmount();
+    const rows = params.getRowsAmount();
+    return Math.ceil(cols * rows * params.difficultLevel);
+  }
+
+  createState = () => {
+    const cols = params.getCollumnsAmount();
+    const rows = params.getRowsAmount();
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount())
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -11,16 +34,9 @@ export default class App extends Component {
         <Text style={styles.instructions}> Tamanho da grade:
           {params.getRowsAmount()}x{params.getCollumnsAmount()}
         </Text>
-        <Field />
-        <Field opened nearMines={1}></Field>
-        <Field opened nearMines={2}></Field>
-        <Field opened nearMines={3}></Field>
-        <Field opened nearMines={6}></Field>
-        <Field mined></Field>
-        <Field mined opened></Field>
-        <Field mined opened exploded></Field>
-        <Field flagged></Field>
-        <Field flagged opened></Field>
+        <View style={styles.board}>
+          <MineField board={this.state.board} />
+        </View>
       </View>
     );
   }
@@ -29,18 +45,10 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end'
+  },
+  board: {
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: '#AAA'
+  }
 });
